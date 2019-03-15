689fdff43b82b7cf611fcf1911223b5a94ade1fb
Oscar Perez <oscar_perez@condenast.com>
2019-03-15 16:52:02 -0400

    Address PR comments.

--- src/main/java/com/blueapron/connect/protobuf/SchemaUtils.java
+++ src/main/java/com/blueapron/connect/protobuf/SchemaUtils.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import javafx.util.Pair;
 import org.apache.kafka.connect.data.Field;
 import org.apache.kafka.connect.data.Schema;
 import org.apache.kafka.connect.data.SchemaAndValue;
@@ -12,23 +13,39 @@ import org.apache.kafka.connect.data.Struct;
 
 public class SchemaUtils {
   static SchemaAndValue flattenNestedStructs(SchemaAndValue connectData, String fieldDelimiter) {
+    Schema origSchema = connectData.schema();
+    Struct origValue = (Struct) connectData.value();
+
+    Pair<Schema, Map<String, Object>> flatSchemaAndValuesMap = createFlatSchemaAndValuesMap(origSchema, origValue, fieldDelimiter);
+
+    Schema flatSchema = flatSchemaAndValuesMap.getKey();
+    Map<String, Object> flatValuesMap = flatSchemaAndValuesMap.getValue();
+    Struct flatStruct = createPopulatedFlatStruct(flatSchema, flatValuesMap);
+    return new SchemaAndValue(flatSchema, flatStruct);
+  }
+
+  private static Pair<Schema, Map<String, Object>> createFlatSchemaAndValuesMap(Schema origSchema, Struct origValue, String fieldDelimiter) {
     SchemaBuilder flatSchemaBuilder = SchemaBuilder.struct();
     Map<String, Object> flatValuesMap = new HashMap<>();
     String fieldNamePrefix = "";
-    Schema dataSchema = connectData.schema();
-    Struct dataValue = (Struct) connectData.value();
-    populateFlatSchema(flatSchemaBuilder, flatValuesMap, fieldDelimiter, fieldNamePrefix, dataSchema, dataValue);
 
-    Schema flatSchema = flatSchemaBuilder.build();
+    // Recursively traverse the original schema and value struct populating our flat schema and
+    // values map.
+    populateFlatSchemaAndValuesMap(flatSchemaBuilder, flatValuesMap, fieldDelimiter, fieldNamePrefix, origSchema, origValue);
+
+    return new Pair<>(flatSchemaBuilder.build(), flatValuesMap);
+  }
+
+  private static Struct createPopulatedFlatStruct(Schema flatSchema, Map<String, Object> flatValuesMap) {
     Struct flatStruct = new Struct(flatSchema);
     for (Entry<String, Object> entry : flatValuesMap.entrySet()) {
       flatStruct.put(entry.getKey(), entry.getValue());
     }
 
-    return new SchemaAndValue(flatSchema, flatStruct);
+    return flatStruct;
   }
 
-  private static void populateFlatSchema(SchemaBuilder flatSchemaBuilder, Map<String, Object> flatValuesMap, String fieldDelimiter, String fieldNamePrefix, Schema schema, Struct value) {
+  private static void populateFlatSchemaAndValuesMap(SchemaBuilder flatSchemaBuilder, Map<String, Object> flatValuesMap, String fieldDelimiter, String fieldNamePrefix, Schema schema, Struct value) {
     List<Field> schemaFields = schema.fields();
     for (Field field : schemaFields) {
       Schema fieldSchema = field.schema();
@@ -37,7 +54,7 @@ public class SchemaUtils {
 
       if (fieldSchema.type() == Schema.Type.STRUCT) {
         String newFieldNamePrefix = fieldNamePrefix + fieldName + fieldDelimiter;
-        populateFlatSchema(flatSchemaBuilder, flatValuesMap, fieldDelimiter, newFieldNamePrefix, fieldSchema, (Struct) fieldValue);
+        populateFlatSchemaAndValuesMap(flatSchemaBuilder, flatValuesMap, fieldDelimiter, newFieldNamePrefix, fieldSchema, (Struct) fieldValue);
       } else {
         String fullFieldName = fieldNamePrefix + fieldName;
         flatSchemaBuilder.field(fullFieldName, fieldSchema);
